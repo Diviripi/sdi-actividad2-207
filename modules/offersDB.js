823d5e3ae1b0da6544a4c4e24192a5dd27a6c4d8
Iván Prieto Labrador <uo258306@uniovi.es>
2019-04-22 16:10:38 +0200

    7 hecho, falta redirecciones cuando la aplicación esté completa

--- modules/offersDB.js
+++ modules/offersDB.js
@@ -1,29 +1,43 @@
 module.exports = {
-	mongo: null,
-	app: null,
-	init: function(app, mongo) {
-		this.mongo = mongo;
-		this.app = app;
-		console.log('Usersdb initialised');
+    mongo: null,
+    app: null,
+    init: function (app, mongo) {
+        this.mongo = mongo;
+        this.app = app;
+        console.log('Usersdb initialised');
     },
-    addOffer:function(offer,functionCallback){
-        this.mongo.MongoClient.connect(this.app.get('db'),function(err,db){
-            if(err){
+    addOffer: function (offer, functionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
+            if (err) {
                 functionCallback(null);
 
-            }else{
-                var collection=db.collection('offers');
-                collection.insert(offer,function(err,result){
-                    if(err){
+            } else {
+                var collection = db.collection('offers');
+                collection.insert(offer, function (err, result) {
+                    if (err) {
                         functionCallback(null);
-                    }
-                    else{
+                    } else {
                         functionCallback(result.ops[0]._id);
                     }
                     db.close();
                 })
             }
         })
-
+    },
+    removeOffer: function (criterio, functionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
+            if (err) {
+                functionCallback(null);
+            } else {
+                let collection = db.collection('offers');
+                collection.remove(criterio, function (err, result) {
+                    if (err) {
+                        functionCallback(null);
+                    } else {
+                        functionCallback(result);
+                    }
+                })
+            }
+        })
     }
 }
--- routes/offersRouter.js
+++ routes/offersRouter.js
@@ -1,35 +1,47 @@
-module.exports = function(app, swig, gestorBD) {
-    app.get("/offers/addOffer",function(req,res){
+module.exports = function (app, swig, gestorBD) {
+    app.get("/offers/addOffer", function (req, res) {
         var respuesta = swig.renderFile('views/newOffer.html', {});
-		res.send(respuesta);
+        res.send(respuesta);
     })
-    app.post("/offers/addOffer",function(req,res){
-        var title=req.body.title;
-        var description=req.body.description;
-        var price=parseFloat(req.body.price);
-        var date= new Date().toLocaleDateString();
-        var highlighted=req.body.highlighted=='on';
-        var user=req.session.usuario;
+    app.post("/offers/addOffer", function (req, res) {
+        var title = req.body.title;
+        var description = req.body.description;
+        var price = parseFloat(req.body.price);
+        var date = new Date().toLocaleDateString();
+        var highlighted = req.body.highlighted == 'on';
+        var user = req.session.usuario;
 
-        var offer={
-            title:title,
-            description:description,
-            price:price,
-            date:date,
-            highlighted:highlighted,
-            user:user
+        var offer = {
+            title: title,
+            description: description,
+            price: price,
+            date: date,
+            highlighted: highlighted,
+            user: user
 
 
         }
 
-        gestorBD.offersDB.addOffer(offer,function(id){
-            if(id==null){
+        gestorBD.offersDB.addOffer(offer, function (id) {
+            if (id == null) {
                 res.redirect("/offers/addOffer");
-            }else{
+            } else {
                 releaseEvents.redirect("/");
             }
         })
         console.log(offer);
         res.redirect("/addOffer");
+    })
+
+    app.get('offers/removeOffer/:id', function (req, res) {
+        let criterio = {_id: gestorBD.mongo.ObjectID(req.params.id)};
+        gestorBD.offersDB.removeOffer(criterio, function (id) {
+            if (id == null) {
+                res.send('error while removing offer');
+            } else {
+                res.send(`Canción ${id} eliminada correctamente`);
+            }
+        })
+
     })
 }
