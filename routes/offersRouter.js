0735ad193287085b6d674785b499bd9db6229578
Iván Prieto Labrador <uo258306@uniovi.es>
2019-04-22 16:47:44 +0200

    8.Mostrar ofertas del usuario hecho

--- routes/offersRouter.js
+++ routes/offersRouter.js
@@ -1,35 +1,53 @@
-module.exports = function(app, swig, gestorBD) {
-    app.get("/offers/addOffer",function(req,res){
+module.exports = function (app, swig, gestorBD) {
+    app.get("/offers/addOffer", function (req, res) {
         var respuesta = swig.renderFile('views/newOffer.html', {});
-		res.send(respuesta);
+        res.send(respuesta);
     })
-    app.post("/offers/addOffer",function(req,res){
-        var title=req.body.title;
-        var description=req.body.description;
-        var price=parseFloat(req.body.price);
-        var date= new Date().toLocaleDateString();
-        var highlighted=req.body.highlighted=='on';
-        var user=req.session.usuario;
+    app.post("/offers/addOffer", function (req, res) {
+        var title = req.body.title;
+        var description = req.body.description;
+        var price = parseFloat(req.body.price);
+        var date = new Date().toLocaleDateString();
+        var highlighted = req.body.highlighted == 'on';
+        var user = req.session.usuario;
 
-        var offer={
-            title:title,
-            description:description,
-            price:price,
-            date:date,
-            highlighted:highlighted,
-            user:user
+        var offer = {
+            title: title,
+            description: description,
+            price: price,
+            date: date,
+            highlighted: highlighted,
+            user: user
 
 
         }
 
-        gestorBD.offersDB.addOffer(offer,function(id){
-            if(id==null){
+        gestorBD.offersDB.addOffer(offer, function (id) {
+            if (id == null) {
                 res.redirect("/offers/addOffer");
-            }else{
-                releaseEvents.redirect("/");
+            } else {
+                res.redirect("/user/offers");
             }
         })
         console.log(offer);
-        res.redirect("/addOffer");
+    })
+
+    app.get('/user/offers', function (req, res) {
+        let user = req.session.usuario;
+        if (user == null) {
+            res.redirect('/login');
+        } else {
+            let criterio = {user: user};
+            let ofertas = gestorBD.offersDB.getOffers(criterio, function (ofertas) {
+                if (ofertas == null) {
+                    res.send('Error al listar');
+                } else {
+                    let respuesta = swig.renderFile('views/bOffers.html', {
+                        ofertas: ofertas
+                    });
+                    res.send(respuesta);
+                }
+            })
+        }
     })
 }
